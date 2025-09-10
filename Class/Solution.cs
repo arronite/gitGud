5265e72a50e88445b2860ae63c30845e77309b39
mamrez <mrz_shirazi@yahoo.com>
2025-09-10 20:18:44 +0330

    delete

--- Class/Solution.cs
+++ Class/Solution.cs
@@ -2,21 +2,5 @@ namespace Class.Solution;
 //TODO: have gay sex
 public class Solution
 {
-    public IList<string> SummaryRanges(int[] nums)
-    {
-        int fIndex = 0;
-        int lIndex = 0;
-        string temp = "";
-        List<string> res = new();
-        for (int i = 0; i < nums.Length; i++)
-        {
-            // if (i == nums.Length - 1){
-            //   res.Add()
-            // }
-            fIndex = nums[i];
-            lIndex = nums[i + 1];
-        }
 
-
-    }
 }
