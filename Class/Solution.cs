fc51dba089c4faf7a50d9ca785bc79245ebe69a5
mamrez <mrz_shirazi@yahoo.com>
2025-09-10 20:29:13 +0330

    testing

--- Class/Solution.cs
+++ Class/Solution.cs
@@ -1,6 +1,19 @@
 namespace Class.Solution;
-//TODO: have gay sex
+
 public class Solution
 {
-
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (dict.ContainsKey(complement))
+            {
+                return new int[] { dict[complement], i };
+            }
+            dict[nums[i]] = i;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
 }
